0021f6c875796ca54f9a81fdc66cf0397988bf3f
Von <145739655+vonrmangaron@users.noreply.github.com>
2026-09-26 16:57:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - On new install, wipes all older caches so no stale assets linger.
 */
 
-const CACHE_NAME = 'prodwise-v29';
+const CACHE_NAME = 'prodwise-v30';
 const CORE_ASSETS = [
   './',
   './index.html',
