fac7adcdc232d4ad876ecc6c9d6ebd89c7a6b1f7
Von <145739655+vonrmangaron@users.noreply.github.com>
2026-09-26 17:22:31 +0800

    Update cache name from v32 to v35

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - On new install, wipes all older caches so no stale assets linger.
 */
 
-const CACHE_NAME = 'prodwise-v32';
+const CACHE_NAME = 'prodwise-v35';
 const CORE_ASSETS = [
   './',
   './index.html',
